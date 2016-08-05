31236efc49eb318435daebbe61f8aaf2ae574a55
Daniel Gorelick <dqgorelick@gmail.com>
2016-08-05 04:25:04 -0400

    adds style lovin part II

--- javascripts/index.js
+++ javascripts/index.js
@@ -3,18 +3,31 @@ chrome.windows.getAll({populate:true},function(windows){
     chrome.windows.getCurrent(function(currentWindow) {
         var count = 1;
         windows.forEach(function(eachWindow) {
+            console.log('each',eachWindow);
             if(eachWindow.id !== currentWindow.id) {
                 chrome.tabs.captureVisibleTab(eachWindow.id, {quality: 50}, function (image) {
                     Mousetrap.bind(count.toString(), function () {
                         selectWindow(eachWindow, toSendId)
                     });
+
                     $("#open-windows").append(function() {
                         console.log('count',count);
-                        var element = $(
-                            "<div class='screenshot' style='background-image:url(" + image + ")''>" +
-                                "<div class='screen-index'>" + count + "</div>" +
-                            "</div>");
-                        count++;
+                        var element;
+                        eachWindow.tabs.forEach(function(tab) {
+                            if (tab.active) {
+                            console.log(tab);
+                                element = $(
+                                    "<div class='screenshot' style='background-image:url(" + image + ")''>" +
+                                        "<div class='title-bar'>" +
+                                            "<img src='" + tab.favIconUrl + "'/>" +
+                                            "<div class='screen-title'>" + tab.title + "</div>" +
+                                        "</div>" +
+                                        "<div class='screen-index'>" + count + "</div>" +
+                                    "</div>"
+                                );
+                                count++;
+                            }
+                        })
                         element.on('click', function() {
                             selectWindow(eachWindow, toSendId);
                         });
