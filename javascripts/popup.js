944ba82252d62fafa33a1abdf706025ad6d65f4a
Daniel Gorelick <dqgorelick@gmail.com>
2016-08-05 15:32:23 -0400

    adds new icons and button

--- javascripts/popup.js
+++ javascripts/popup.js
@@ -15,3 +15,19 @@ $("#pop").click(function() {
 $("#send").click(function() {
     port.postMessage("send");
 });
+
+var bonusClicked = 0;
+$("#bonus").click(function() {
+    bonusClicked+=1;
+    if (bonusClicked === 1) {
+        $("#bonus").html("stop it");
+    } else if (bonusClicked === 2) {
+        $("#bonus").html("I'm warning you");
+    } else if (bonusClicked === 3) {
+        $("#bonus").html("last warning...");
+    }
+    if (bonusClicked > 3) {
+        $("#bonus").html("explode!!");
+        console.log('Biggie put the explode function here');
+    }
+})
