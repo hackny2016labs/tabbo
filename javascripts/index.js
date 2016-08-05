7cb6fdbe791ef9d0732a1944d7ddf7ea99d63285
Daniel Gorelick <dqgorelick@gmail.com>
2016-08-05 03:09:37 -0400

    giving it some css lovin

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,55 +1,22 @@
-(function(){
-    $('.test_1').on('click',function() {
-        chrome.tabs.getSelected(function(tab) {
-            chrome.tabs.move(tab.id, {windowId: window.id, index: tab.index + 1}, function(){
-                console.log("done!");
-            });
-        });
-    });
-    $('.test_2').on('click',function() {
-        chrome.windows.getAll({populate:true},function(windows){
-            windows.forEach(function(window){
-                chrome.tabs.captureVisibleTab(window.id, {quality: 50}, function (image) {
+var toSendId = window.location.hash.slice(1);
+chrome.windows.getAll({populate:true},function(windows){
+    chrome.windows.getCurrent(function(currentWindow) {
+        windows.forEach(function(eachWindow) {
+            if(eachWindow.id !== currentWindow.id) {
+                chrome.tabs.captureVisibleTab(eachWindow.id, {quality: 50}, function (image) {
                     $( "#open-windows" ).append(function() {
-                        return $("<img class='screenshot_move' src=" + image + " width='400'>").on('click', function() {
-                            moveHandler(window.id);
+                        return $("<div class='screenshot' style='background-image:url("+ image + ")''>").on('click', function() {
+                            chrome.tabs.getSelected(function(tab){
+                                chrome.tabs.remove(tab.id);
+                            })
+                            sendTab(eachWindow.id, parseInt(toSendId));
                         });
                     });
                 });
-            });
+            }
         });
     });
-    $('.test_3').on('click',function() {
-        chrome.tabs.getSelected(function(tab) {
-            chrome.tabs.query({currentWindow: true}, function(tabs) {
-                var nextTab = tabs[tab.index + 1];
-                chrome.tabs.update(nextTab.id, {active: true}, function() {
-                    console.log("done!");
-                });
-            });
-        });
-    });
-    $('.test_4').on('click', function() {
-        popOffWindow();
-    })
-})();
-
-
-function popOffWindow() {
-    chrome.tabs.getSelected(function(tab){
-        chrome.windows.create({tabId: tab.id}, function() {
-            console.log('popped off!');
-        });
-    })
-}
-
-function moveHandler(window_id) {
-    chrome.tabs.getSelected(function(tab) {
-        chrome.tabs.move(tab.id, {windowId: window_id, index: -1}, function(){
-            console.log("done!");
-        });
-    });
-}
+});
 
 function sendTab(windowId, tabId) {
     chrome.tabs.move(tabId, {windowId: windowId, index: -1}, function() {
@@ -57,24 +24,56 @@ function sendTab(windowId, tabId) {
     })
 }
 
-var port = chrome.extension.connect({name: "Sample Communication"});
-// port.postMessage("Hi BackGround");
-// port.onMessage.addListener(function(msg) {
-    console.log('window.location.hash.slice(1',window.location.hash.slice(1));
-    var toSendId = window.location.hash.slice(1);
-    chrome.windows.getAll({populate:true},function(windows){
-        windows.forEach(function(window){
-            chrome.tabs.captureVisibleTab(window.id, {quality: 50}, function (image) {
-                $( "#open-windows" ).append(function() {
-                    return $("<img class='screenshot_move' src=" + image + " width='400'>").on('click', function() {
-                        chrome.tabs.getSelected(function(tab){
-                            console.log('tab.id',tab.id);
-                            chrome.tabs.remove(tab.id);
-                        })
-                        sendTab(window.id, parseInt(toSendId));
-                    });
-                });
-            });
-        });
-    });
-// });
+
+// (function(){
+//     $('.test_1').on('click',function() {
+//         chrome.tabs.getSelected(function(tab) {
+//             chrome.tabs.move(tab.id, {windowId: window.id, index: tab.index + 1}, function(){
+//                 console.log("done!");
+//             });
+//         });
+//     });
+//     $('.test_2').on('click',function() {
+//         chrome.windows.getAll({populate:true},function(windows){
+//             windows.forEach(function(window){
+//                 chrome.tabs.captureVisibleTab(window.id, {quality: 50}, function (image) {
+//                     $( "#open-windows" ).append(function() {
+//                         return $("<img class='screenshot_move' src=" + image + " width='400'>").on('click', function() {
+//                             moveHandler(window.id);
+//                         });
+//                     });
+//                 });
+//             });
+//         });
+//     });
+//     $('.test_3').on('click',function() {
+//         chrome.tabs.getSelected(function(tab) {
+//             chrome.tabs.query({currentWindow: true}, function(tabs) {
+//                 var nextTab = tabs[tab.index + 1];
+//                 chrome.tabs.update(nextTab.id, {active: true}, function() {
+//                     console.log("done!");
+//                 });
+//             });
+//         });
+//     });
+//     $('.test_4').on('click', function() {
+//         popOffWindow();
+//     })
+// })();
+
+
+// function popOffWindow() {
+//     chrome.tabs.getSelected(function(tab){
+//         chrome.windows.create({tabId: tab.id}, function() {
+//             console.log('popped off!');
+//         });
+//     })
+// }
+
+// function moveHandler(window_id) {
+//     chrome.tabs.getSelected(function(tab) {
+//         chrome.tabs.move(tab.id, {windowId: window_id, index: -1}, function(){
+//             console.log("done!");
+//         });
+//     });
+// }
