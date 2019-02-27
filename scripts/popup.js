598b4a56e8ac060adf379ae9036919a1278ddca0
Scott Reed <scott.reed@dynamiccatholic.com>
2019-02-27 11:05:11 -0500

    remove bonus

--- scripts/popup.js
+++ scripts/popup.js
@@ -19,23 +19,3 @@ document.querySelector('#send').addEventListener('click', () => {
 document.querySelector('#send').addEventListener('click', () => {
 	port.postMessage('send');
 });
-
-let bonusClicked = 0;
-const bonusElem = document.querySelector('#bonus');
-
-document.querySelector('#bonus').addEventListener('click', () => {
-	bonusClicked += 1;
-
-	if (bonusClicked === 1) {
-		bonusElem.innerHTML('stop it');
-	} else if (bonusClicked === 2) {
-		bonusElem.innerHTML('I\'m warning you');
-	} else if (bonusClicked === 3) {
-		bonusElem.innerHTML('last warning...');
-	}
-
-	if (bonusClicked > 3) {
-		bonusElem.innerHTML('explode!!');
-		port.postMessage('explode');
-	}
-});
